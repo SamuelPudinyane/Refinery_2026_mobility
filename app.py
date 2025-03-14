2d430ab30b19e01aea7c4cd45b90c500379e1e97
Tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2025-03-14 09:12:56 +0200

    messages

--- app.py
+++ app.py
@@ -378,6 +378,8 @@ def admin_view_answers():
     # For GET requests, return all answers
     answers = get_all_answered_questions()
     print("default ",answers)
+    for item in answers:
+        print(item['checklist_answers'])
     return render_template("admin_view_answers.html", answers=answers, plant_sections=plant_sections)
 
 
